b5764ad67a85a4b2def98a9d3011b43e0d01eb9e
TirolJPN <tirol.jpn@gmail.com>
2019-05-20 15:10:38 +0900

    delete lexical_clustering.py because of unnecessary

--- lexical_clustering.py
+++
@@ -1,9 +0,0 @@
-from my_library import key
-
-
-class ExecLexicalCluster():
-    # problem_idを引数に対して，クラスタリングを実行する
-    def __init__(self, problem_id):
-        # 各定数の初期化
-        
-        self.PATH_VECTOR_FILES
