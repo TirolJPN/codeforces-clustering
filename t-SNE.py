950cb608ba8c446c40c20e3b9d23353334a829cb
TirolJPN <tirol.jpn@gmail.com>
2019-08-31 18:51:54 +0900

    change t-sne.py

--- t-SNE.py
+++ t-SNE.py
@@ -1,10 +1,21 @@
-from bhtsne import tsne
-from sklearn.datasets import load_iris
 import matplotlib.pyplot as plt
-import numpy as np
+from sklearn import datasets
+from sklearn.manifold import TSNE
 
-iris = load_iris()
-Y = tsne(iris.data)
-plt.scatter(Y[:, 0], Y[:, 1], c=iris.target)
-# plt.show()
-plt.savefig('figure.png')   
+digits = datasets.load_digits()
+
+# print digits.data.shape
+# (1797, 64)
+
+# print digits.target.shape
+# (1797,)
+
+X_reduced = TSNE(n_components=2, random_state=0).fit_transform(digits.data)
+
+# print X_reduced.shape
+# (1797, 2)
+
+plt.scatter(X_reduced[:, 0], X_reduced[:, 1], c=digits.target)
+plt.colorbar()
+plt.savefig('figure.png')
+# <matplotlib.colorbar.Colorbar at 0x7ff21173ee90>
