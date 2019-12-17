7a3f121b4309b71584b46a68ef4c70780e3371f2
TirolJPN <tirol.jpn@gmail.com>
2019-12-17 17:20:41 +0900

    fix path bugs and change directories

--- jobs/clustering/my_library/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import fetching_exec_time_and_memory
-from . import Connector
-from . import key
---
+++ jobs/my_library/__init__.py
@@ -0,0 +1,3 @@
+from . import fetching_exec_time_and_memory
+from . import Connector
+from . import key
